a8b1d126b3d175dc475784a5328c9499509fc1c6
Katie P <35932711+devkatie@users.noreply.github.com>
2022-06-12 15:19:37 -0400

    html/css prepping for php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+    <title>Registration Form Using HTML5 and CSS3</title>
+</head>
+<body>
+    
+    <div class="signup-form">
+        <img src="user.png">
+        <form action="index.html" method="post">
+            <input type="text" placeholder="User Name" class="txt">
+            <input type="email" placeholder="Email" class="txt">
+            <input type="password" placeholder="Password" class="txt">
+            <input type="button" value="Create an Account" class="btn">
+            <a href="#">Already Have an Account</a>
+        </form>
+    </div>
+
+</body>
+</html>
