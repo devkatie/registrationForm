5709b4e1ab6ec5de566215cfb7051a836177c0e6
Katie P <35932711+devkatie@users.noreply.github.com>
2022-06-13 22:55:04 -0400

    updated process.php + successful insert

--- process.php
+++ process.php
@@ -1,9 +1,40 @@
 <?php
 
+    require_once('connection.php');
+    
     if(isset($_POST['btn-save'])) {
-        
-        echo 'Working';
+    
+        $Username = mysqli_real_escape_string($con, $_POST['Username']);
+        $Email = mysqli_real_escape_string($con, $_POST['Email']);
+        $Password = mysqli_real_escape_string($con, $_POST['Password']);
+        $CPassword = mysqli_real_escape_string($con, $_POST['CPass']);
 
+        if(empty($Username) || empty($Email) || empty($Password) || empty($CPassword)) {
+
+            echo 'Please fill in all the fields to continue';
+
+        } else if($Password != $CPassword) {
+
+            echo 'Passwords do not match! Please retry...';
+
+        } else {
+
+            $Pass = md5($Password);
+            $sql = "insert into users (Username, Email, Password) values ('$Username', '$Email', '$Pass')";
+            $result = mysqli_query($con, $sql);
+
+            if($result) {
+
+                echo 'Your account has been saved successfully in the database!';
+
+            } else {
+
+                echo 'Please check your query';
+
+            }
+
+        }
+        
     }
 
 ?>
